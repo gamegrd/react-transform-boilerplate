50d0c4325ad31c8e42a1855ed31d1932cf0a761b
LiuXing <LiuXing@134.local>
2016-01-20 20:12:56 +0800

    add Amaze UI

--- src/App.js
+++ src/App.js
@@ -1,43 +1,45 @@
 import React, { Component } from 'react';
 import { NICE, SUPER_NICE } from './colors';
+import {Button} from 'amazeui-react';
 
 require('./main.css');
 
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 };
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
 
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
+    tick() {
+        this.setState({
+            counter: this.state.counter + this.props.increment
+        });
+    }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
 
-  render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
-  }
+    render() {
+        return (
+            <h1 style={{ color: this.props.color }}>
+            Counter ({this.props.increment}): {this.state.counter}
+            </h1>
+        );
+    }
 }
 
 export class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className="header">  <div> this is header  </div> </div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+            <Button>Button</Button>
+            <div className="header">  <div> this is header  </div> </div>
+            <Counter increment={1} color={NICE} />
+            <Counter increment={5} color={SUPER_NICE} />
+            </div>
+        );
+    }
 }
